cdf4938dea2a0a2bceec6eb27533063652a195e5
Geneva Cheng <70555640+genevacheng@users.noreply.github.com>
2024-04-28 00:08:39 -0400

    Fixed

--- stock2.js
+++ stock2.js
@@ -9,6 +9,7 @@ http.createServer(function (req, res) {
     var page = url.parse(req.url).pathname;
 
     if (page == "/") {
+        // home page
         res.writeHead(200, {"Content-Type": "text/html"});
         var file = "home.html";
         fs.readFile(file, function (err, txt) {
@@ -16,8 +17,10 @@ http.createServer(function (req, res) {
             res.end();
         });
     } else if (page == "/process") {
+        // process page
         var qobj = url.parse(req.url, true).query;
 
+        // connect to database
         var MongoClient = mongo.MongoClient;
         var connectionString = "mongodb+srv://genevacheng:Hyperspace12@cluster0.swouo2s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
         MongoClient.connect(connectionString, function (err, account) {
@@ -26,9 +29,11 @@ http.createServer(function (req, res) {
                 return;
             }
 
+            // select collection from database
             var db = account.db("Stock");
             var collection = db.collection("PublicCompanies");
 
+            // run query
             var query = {[qobj.type]: qobj.query};
             collection.find(query).toArray()
                 .then(results => {
@@ -38,4 +43,4 @@ http.createServer(function (req, res) {
 
         res.end();
     }
-}).listen(8080);
+}).listen(port);
